c378292a1c7eb124cca6dbbfd38c923424f7f9aa
Remi Debay <remi@ks01.r-2b.net>
2013-03-21 16:37:42 +0100

    Ajout script de heartbeat

---
+++ scripts/heartbeat.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#########################################################
+#   Filename   : populate.sh                             #
+#   Description: TODO   #
+#########################################################
+
+PROGNAME="$(basename $0)"
+quiet=false
+
+
+url="http://my-mon.appspot.com/event"
+testname="heartbeat"
+level="0"
+
+
+set -u
+set -e
+
+# Prints help message
+function showhelp {
+    cat >&2 <<- EOF 
+Usage: $PROGNAME [OPTION] ...
+Options:
+    -h  prints this help message.
+EOF
+}
+
+# Prints log message to default output
+function log {
+    local level="$1"
+    local message="$2"
+
+    $quiet || echo "$(date +"%F %T" ) $PROGNAME: $message" 
+}
+
+while getopts "t:H:qh" opt
+do
+    case $opt in
+        q)  quiet=true
+            ;;
+        h)  showhelp
+            exit 1
+            ;;
+        *)  showhelp
+            exit 1
+            ;;
+    esac
+done
+
+data="default"
+
+timestamp="$(date +%s)"
+request="reference=$HOSTNAME&test=$testname&timestamp=$timestamp"
+request="$request&level=$level&data=$data"
+
+curl "$url?$request"
+
